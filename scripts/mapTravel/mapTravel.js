f73049971551b93fa60c48639302a90c0ab282b2
Bastien <legallbastien305@gmail.com>
2022-12-02 03:38:29 +0100

    mapTravel update

--- scripts/mapTravel/mapTravel.js
+++ scripts/mapTravel/mapTravel.js
@@ -18,53 +18,54 @@ function createMap() {
     row.appendChild(column1);
     row.appendChild(column2);
 
-    if (i == 0) {
-      var nodeRepos = createNodeAlways();
-      nodeRepos.className = "start";
-      column1.appendChild(nodeRepos);
-      container.appendChild(row);
-      continue;
-    }
-
-    if (i == numberRows - 1) {
-      var nodeRepos = createNodeAlways();
-      nodeRepos.className = "end";
+    if (i == 0 || i == numberRows - 1) {
+      var nodeRepos = createNodeAlways(i);
+      nodeRepos.classList.add("center");
       column1.appendChild(nodeRepos);
       container.appendChild(row);
       continue;
     }
 
     if (i % 5 == 0) {
-      var nodeRepos = createNodeAlways();
+      var nodeRepos = createNodeAlways(i);
       nodeRepos.className = "repos";
       column1.appendChild(nodeRepos);
       container.appendChild(row);
       continue;
     }
 
-    var node = createNode();
+    var node = createNode(i);
     if (node) {
       column.appendChild(node);
+      node.classList.add("left");
     }
 
-    var node1 = createNode();
+    var node1 = createNode(i);
     if (node1) {
       column1.appendChild(node1);
+      node1.classList.add("center");
     }
 
-    var node2 = createNode();
+    var node2 = createNode(i);
     if (node2) {
       column2.appendChild(node2);
+      node2.classList.add("right");
     }
 
     if (!node && !node1 && !node2) {
       var random = Math.random();
       if (random < 0.33) {
-        column.appendChild(createNodeAlways());
+        var node = createNodeAlways(i);
+        column.appendChild(node);
+        node.classList.add("left");
       } else if (random < 0.66) {
-        column1.appendChild(createNodeAlways());
+        var node1 = createNodeAlways(i);
+        column1.appendChild(node1);
+        node1.classList.add("center");
       } else {
-        column2.appendChild(createNodeAlways());
+        var node2 = createNodeAlways(i);
+        column2.appendChild(node2);
+        node2.classList.add("right");
       }
     }
 
@@ -74,8 +75,8 @@ function createMap() {
 
 createMap();
 
-function createNode() {
-  var node = createNodeAlways();
+function createNode(i) {
+  var node = createNodeAlways(i);
 
   var random = Math.random();
   if (random < 0.43) {
@@ -84,17 +85,66 @@ function createNode() {
   return null;
 }
 
-function createNodeAlways() {
+function createNodeAlways(i) {
   const node = document.createElement("div");
-  var randomType = Math.random();
-  if (randomType < 0.7) {
-    node.className = "combat";
-  } else {
-    node.className = "evenement";
+  const a = document.createElement("a");
+  const img = document.createElement("img");
+
+  node.className = i;
+
+  a.appendChild(img);
+  node.appendChild(a);
+
+  if(i == 0){
+    node.classList.add("node");
+    node.classList.add("start");
+    img.src = "../assets/images/start.png";
+    img.width = 50;
+    img.height = 50;
+
+    return node;
   }
 
-  var randomTranslateY = Math.random();
-  var randomTranslateX = Math.random();
+  if(i == 14){
+    node.classList.add("node");
+    node.classList.add("end");
+    img.src = "../assets/images/start.png";
+    img.width = 50;
+    img.height = 50;
+
+    return node;
+  }
+
+  if(i % 5 == 0){
+    node.classList.add("node");
+    node.classList.add("repos");
+    img.src = "../assets/images/repos.png";
+    img.width = 50;
+    img.height = 50;
+
+    return node;
+  }
+
+  var randomType = Math.random();
+  if (randomType < 0.7) {
+    node.classList.add("node");
+    node.classList.add("combat");
+    a.href="../combat/combat.php";
+    img.src = "../../assets/images/combat.png";
+    img.width = 50;
+    img.height = 50;
+  } else {
+    node.classList.add("node");
+    node.classList.add("evenement");
+    a.href="../combat/evenement.php";
+    img.src = "../../assets/images/evenement.png";
+    img.width = 50;
+    img.height = 50;
+  }
+
+  var randomTranslateY = Math.random() * 0.4 + 0.25;
+  var randomTranslateX = Math.random() * 0.40 + 0.25;
+
 
   var randomXPos = Math.random();
   if (randomXPos < 0.5) {
@@ -124,8 +174,25 @@ function createNodeAlways() {
   return node;
 }
 
-function createLinkBetweenNodes() {
-  const link = document.createElement("div");
-  link.className = "link";
-  return link;
+function saveMap() {
+  var allCombat = document.getElementsByClassName("combat");
+  var allEvenement = document.getElementsByClassName("evenement");
+  var allRepos = document.getElementsByClassName("repos");
+
+  console.log(allCombat);
+  console.log(allEvenement);
+  console.log(allRepos);
+
+  // Save allCombat in URL and position
+  var allCombatURL = "";
+  var allCombatPosition = "";
+  for (var i = 0; i < allCombat.length; i++) {
+    // split allCombat with space and save [0] in allCombatURL and [3] in allCombatPosition
+    var split = allCombat[i].innerHTML.split(" ");
+    allCombatURL += split[0] + " ";
+    allCombatURL   += split[3] + " ";
+  }
+  console.log(allCombatURL);
 }
+
+saveMap();
