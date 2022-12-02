c73a61fdf0516e332056800f6353fcd0a16c7238
Noé Chouteau <osolarix@gmail.com>
2022-12-02 06:21:40 +0100

    Menu complété

--- scripts/home.js
+++ scripts/home.js
@@ -2,8 +2,33 @@ let bookOS = new Audio("assets/sounds/book.wav");
 
 function lancerMenu(){
   
-    var elem = document.getElementById("book");
-    elem.setAttribute("src", "assets/images/book_open.png");
+    document.getElementById("bookC").classList.add('bookAnim');
     bookOS.play();
-    
+
+    setTimeout(function() {
+
+        var elem = document.getElementById("bookC");
+        elem.style.top = '-100vh';
+        document.getElementById("bookC").style.pointerEvents = "none";
+        
+        setTimeout(function() {
+        var elem = document.getElementById("bigBookC");
+        elem.classList.add('bigBookAnim');
+
+        setTimeout(function() {
+            var elem = document.getElementById("bigBookC");
+            elem.style.top = '5vh';
+
+            setTimeout(function() {
+                document.getElementById("croix1C").style.display = 'flex';
+                document.getElementById("croix2C").style.display = 'flex';
+                document.getElementById("croix3C").style.display = 'flex';
+                document.getElementById("croix4C").style.display = 'flex';
+            }, 1000);
+
+            },1000);
+
+        },700);
+
+    }, 200);
     }
